ad3db69238e59e5ae915613ffebf929cb4934a2d
LM-Firefly <firefly.lzh@gmail.com>
2020-10-29 18:04:06 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -360,7 +360,7 @@ export default {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
           "firefly-sub.vercel.app": "https://firefly-sub.vercel.app/sub?",
           "firefly-sub.herokuapp.com": "https://firefly-sub.herokuapp.com/sub?",
-          "sub.ccsub.site(CordCloud机场提供)": "https://sub.ccsub.site/sub?",
+          "sub.ccsub.site(CordCloud机场提供)": "https://subweb.ccsub.site/sub?",
           "subcon.dlj.tf(subconverter作者提供1)": "https://subcon.dlj.tf/sub?",
           "subconverter-web.now.sh": "https://subconverter-web.now.sh/sub?",
           "subconverter.herokuapp.com":
@@ -372,7 +372,7 @@ export default {
           { value: "http://localhost:25500/sub?" },
           { value: "https://firefly-sub.vercel.app/sub?" },
           { value: "https://firefly-sub.herokuapp.com/sub?" },
-          { value: "https://sub.ccsub.site/sub?" },
+          { value: "https://subweb.ccsub.site/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://subconverter-web.now.sh/sub?" },
           { value: "https://subconverter.herokuapp.com/sub?" },
