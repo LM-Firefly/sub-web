c9a0f824bd8b0ac12ce97936c9a5cc2f0dc7d26e
LM-Firefly <firefly.lzh@gmail.com>
2023-06-19 18:35:14 +0800

    retry
    Signed-off-by: LM-Firefly <firefly.lzh@gmail.com>

--- vue.config.js
+++ vue.config.js
@@ -14,19 +14,20 @@ module.exports = {
     },
   },
 
-  chainWebpack: (config) => {
+  chainWebpack: ( config ) =>
+  {
     // set svg-sprite-loader
-    config.module.rule("svg").exclude.add(resolve("src/icons")).end();
+    config.module.rule( "svg" ).exclude.add( resolve( "src/icons" ) ).end();
     config.module
-      .rule("icons")
-      .test(/\.svg$/)
-      .include.add(resolve("src/icons"))
+      .rule( "icons" )
+      .test( /\.svg$/ )
+      .include.add( resolve( "src/icons" ) )
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
-      .options({
+      .use( "svg-sprite-loader" )
+      .loader( "svg-sprite-loader" )
+      .options( {
         symbolId: "icon-[name]",
-      })
+      } )
       .end();
   },
 
@@ -38,11 +39,11 @@ module.exports = {
       importWorkboxFrom: "local",
       importsDirectory: "js",
       navigateFallback: "/",
-      navigateFallbackBlacklist: [/\/api\//],
+      navigateFallbackBlacklist: [ /\/api\// ],
     },
   },
-
-  rules: {
-    "vue/multi-word-component-names": "off",
-  },
 };
+
+  rules : {
+    "vue/multi-word-component-names": "off"
+  }
